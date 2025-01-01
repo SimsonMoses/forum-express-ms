b73b7fe26e288b2dbf1cf1664cd7c79fa6bf719a
Simson M <sims91502@gmail.com>
2025-01-01 06:43:59 +0530

    feat: init forum post entity

---
+++ models/forum/ForumPost.js
@@ -0,0 +1,46 @@
+import {Model} from "sequelize";
+
+
+export default (sequelize, DataTypes)=>{
+    class ForumPost extends Model{
+
+    }
+    ForumPost.init({
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        forumId:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        userId:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        },
+        title:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        content:{
+            type:DataTypes.TEXT,
+            allowNull:false
+        },
+        isPinned:{
+            type:DataTypes.BOOLEAN,
+            defaultValue:false
+        },
+        isLocked:{
+            type:DataTypes.BOOLEAN,
+            defaultValue:false
+        }
+    },{
+        sequelize,
+        timestamps:true,
+        modelName:'ForumPost',
+        tableName:'forum_posts'
+    })
+    return ForumPost;
+
+}
