35abb24de68e6ca1e31d14cff23e851d23fc1a27
Simson M <sims91502@gmail.com>
2025-01-09 05:48:14 +0530

    feat: comment delete

--- routes/forum/forumPostRoute.js
+++ routes/forum/forumPostRoute.js
@@ -6,7 +6,12 @@ import {
 } from "../../controller/forum/forumPostController.js";
 import express from "express";
 import {getPostLikeCount, like} from "../../controller/forum/postLikeController.js";
-import {commentPost, getAllComments, updatePostComment} from "../../controller/forum/postCommentController.js";
+import {
+    commentPost,
+    deletePostComment,
+    getAllComments,
+    updatePostComment
+} from "../../controller/forum/postCommentController.js";
 
 
 const router = express.Router();
@@ -14,6 +19,6 @@ const router = express.Router();
 router.use
 router.route('/').post(createForumPost).get(fetchForumPosts).put(updateForumPost).delete(deleteForumPost);
 router.route('/like').post(like).get(getPostLikeCount);
-router.route('/comment').post(commentPost).get(getAllComments).put(updatePostComment);
+router.route('/comment').post(commentPost).get(getAllComments).put(updatePostComment).delete(deletePostComment);
 
 export default router;
