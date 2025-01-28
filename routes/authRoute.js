84420d98315508f275cf40b8e39ea01781cfc442
Simson M <sims91502@gmail.com>
2025-01-28 19:28:00 +0530

    debug: endpoint not found

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,4 +6,10 @@ const authRoute = express.Router();
 authRoute.post('/register', createUser);
 authRoute.post('/login', login);
 
+authRoute.route('/').get((req, res) => {
+    res.status(200).json({
+        message: 'Welcome to Auth Route'
+    })
+})
+
 export default authRoute;
