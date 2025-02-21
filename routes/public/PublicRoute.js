d8e04dbd3883a9f0b2cd0a0d0714175b7aaf2f94
Simson M <sims91502@gmail.com>
2025-02-21 20:05:42 +0530

    feat: public Post comment

--- routes/public/PublicRoute.js
+++ routes/public/PublicRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import PostRoute from "./PostRoute.js";
+import PostCommentRoute from "./postCommentRoute.js";
 
 
 const publicRouter = express.Router();
 
+publicRouter.use('/post/comment',PostCommentRoute);
 publicRouter.use('/post',PostRoute);
 
 export default publicRouter;
